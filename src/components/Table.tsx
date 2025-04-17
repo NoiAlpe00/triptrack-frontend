13f70c2a415d2253ee91cca70cdaaacf1f5aa1cf
NoiAlpe <honesto.alpe@awsys-i.com>
2025-04-17 21:50:29 +0800

    admin now available, ready for integration.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,31 @@
 import { Paper } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import { useRef, useEffect } from "react";
 
-export default function CustomTable({ rows, columns, type = "dashboard"}: { rows: any; columns: any; type: string}) {
-  const paginationModel = { page: 0, pageSize: 30 };
+export default function CustomTable({ rows, columns, type = "dashboard" }: { rows: any; columns: any; type: string }) {
+  const gridRef = useRef<any>(null);
+  const paginationModel = { page: 0, pageSize: 10 };
+
+  // Resize after visible
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      gridRef.current?.apiRef?.current?.resize();
+    }, 200);
+    return () => clearTimeout(timeout);
+  }, [rows]); // or pass an external key if you prefer
+
   return (
-    <Paper sx={{ height: type == "dashboard" ? 350 : 700, width: "100%" }}>
+    <Paper sx={{ height: type == "dashboard" ? 350 : type == "settings" ? 644 : 700, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[10, 20, 50, 100]}
         rowHeight={38}
         sx={{ border: 0 }}
         rowSelection={false}
+        ref={gridRef}
       />
     </Paper>
-  );
+  )
 }
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import CustomToast from "../components/Toast";
+import { Button, Col, Container, FloatingLabel, Form, Row, Image, Nav, Tab } from "react-bootstrap";
+import CustomTable from "../components/Table";
+import Eye from "../assets/svgs/eye.svg";
 
 export default function AdminPage() {
   const [showToast, setShowToast] = useState<boolean>(false);
@@ -13,10 +16,252 @@ export default function AdminPage() {
     }
   }, []);
 
+  const departmentCols = [
+    {
+      field: "view",
+      headerName: "",
+      width: 40,
+      minWidth: 40,
+      maxWidth: 40,
+      sortable: false,
+      renderCell: (params: any) => (
+        <>
+          <Row className="d-flex">
+            <Col className="px-1">
+              <Button size="sm" className="w-100 align-text-center" onClick={() => console.log(params.row)}>
+                <Image className="" src={Eye} />
+              </Button>
+            </Col>
+          </Row>
+        </>
+      ),
+      // valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+    { field: "name", headerName: "Name", flex: 3 },
+    { field: "isDeleted", headerName: "Deleted?", flex: 1 },
+  ];
+  const departmentRows = [
+    { id: 1, name: "Department 1", isDeleted: false },
+    { id: 2, name: "Department 2", isDeleted: true },
+    { id: 3, name: "Department 3", isDeleted: false },
+  ];
+  const userCols = [
+    {
+      field: "view",
+      headerName: "",
+      width: 40,
+      minWidth: 40,
+      maxWidth: 40,
+      sortable: false,
+      renderCell: (params: any) => (
+        <>
+          <Row className="d-flex">
+            <Col className="px-1">
+              <Button size="sm" className="w-100 align-text-center" onClick={() => console.log(params.row)}>
+                <Image className="" src={Eye} />
+              </Button>
+            </Col>
+          </Row>
+        </>
+      ),
+      // valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 2 },
+    { field: "userType", headerName: "Type", flex: 1 },
+    { field: "contactNumber", headerName: "Contact No.", flex: 1 },
+    { field: "status", headerName: "Status", flex: 1 },
+  ];
+  const userRows = [
+    {
+      id: 1,
+      name: "Emedu 1",
+      email: "email@emec.com",
+      userType: "staff",
+      contactNumber: "123456",
+      status: "Active",
+    },
+    {
+      id: 2,
+      name: "Emedu 2",
+      email: "email2@emec.com",
+      userType: "staff",
+      contactNumber: "123456",
+      status: "Incative",
+    },
+    {
+      id: 3,
+      name: "Emedu 3",
+      email: "email3@emec.com",
+      userType: "staff",
+      contactNumber: "123456",
+      status: "Active",
+    },
+  ];
+
+  const vehicleCols = [
+    {
+      field: "view",
+      headerName: "",
+      width: 40,
+      minWidth: 40,
+      maxWidth: 40,
+      sortable: false,
+      renderCell: (params: any) => (
+        <>
+          <Row className="d-flex">
+            <Col className="px-1">
+              <Button size="sm" className="w-100 align-text-center" onClick={() => console.log(params.row)}>
+                <Image className="" src={Eye} />
+              </Button>
+            </Col>
+          </Row>
+        </>
+      ),
+      // valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+    { field: "model", headerName: "Model", flex: 2 },
+    { field: "plate", headerName: "Plate", flex: 1.5 },
+    { field: "seats", headerName: "Seats", flex: 0.5 },
+    { field: "isDeleted", headerName: "Deleted?", flex: 1 },
+  ];
+  const vehicleRows = [
+    { id: 1, model: "BMW EMEDU", plate: "B31267", seats: 6, isDeleted: false },
+    { id: 2, model: "BMW EMEDI", plate: "B31268", seats: 6, isDeleted: false },
+    { id: 3, model: "BMW EMEDE", plate: "B31269", seats: 6, isDeleted: false },
+  ];
+  const checklistCols = [
+    {
+      field: "view",
+      headerName: "",
+      width: 40,
+      minWidth: 40,
+      maxWidth: 40,
+      sortable: false,
+      renderCell: (params: any) => (
+        <>
+          <Row className="d-flex">
+            <Col className="px-1">
+              <Button size="sm" className="w-100 align-text-center" onClick={() => console.log(params.row)}>
+                <Image className="" src={Eye} />
+              </Button>
+            </Col>
+          </Row>
+        </>
+      ),
+      // valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+    { field: "title", headerName: "Name", flex: 3 },
+    { field: "isDeleted", headerName: "Deleted?", flex: 1 },
+  ];
+  const checklistRows = [
+    { id: 1, title: "Gas", isDeleted: false },
+    { id: 2, title: "Tires", isDeleted: true },
+    { id: 3, title: "Aircon", isDeleted: false },
+  ];
+
+  const [activeTab, setActiveTab] = useState("department");
+
   return (
-    <div>
-      ADMIN!!!!
-      <CustomToast header={"Login"} body={"Login Unsuccessful"} time={"Just now"} show={showToast} setShow={setShowToast} variant={"success"} />;
-    </div>
+    <Container fluid>
+      <Row className="pt-5 pb-3 px-1">
+        <Col lg={6} className="mb-2 px-4">
+          <Row className="d-flex align-items-center mb-2">
+            <Col lg={6} className="">
+              <h2 className="text-primary thin-text text-start">All Users</h2>
+            </Col>
+            <Col lg={6} className="">
+              <FloatingLabel controlId="floatingSelect" label="Status" className="small-input">
+                <Form.Select name="statusFilter" aria-label="Floating label select example">
+                  <option>Open this select menu</option>
+                  <option value="all">All</option>
+                  <option value="ongoing">Ongoing</option>
+                  <option value="upcoming">Upcoming</option>
+                  <option value="past">Past</option>
+                </Form.Select>
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Row>
+            <CustomTable rows={userRows} columns={userCols} type="admin" />
+          </Row>
+        </Col>
+        <Col lg={6} className="px-4">
+          <Tab.Container activeKey={activeTab} onSelect={(k: any) => setActiveTab(k)}>
+            <Nav variant="pills" className="mb-3 justify-content-start">
+              <Nav.Item>
+                <Nav.Link eventKey="department">Department</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="vehicle">Vehicle</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="checklist">Checklist</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+              <Tab.Pane eventKey="department">
+                <Row className="d-flex align-items-center mb-2">
+                  <Col lg={6} className="">
+                    <h2 className="text-primary thin-text text-start">All Department</h2>
+                  </Col>
+                  <Col lg={6} className="">
+                    <FloatingLabel controlId="floatingSelect" label="Status" className="small-input">
+                      <Form.Select name="statusFilter" aria-label="Floating label select example">
+                        <option>Open this select menu</option>
+                        <option value="all">All</option>
+                        <option value="ongoing">Ongoing</option>
+                        <option value="upcoming">Upcoming</option>
+                        <option value="past">Past</option>
+                      </Form.Select>
+                    </FloatingLabel>
+                  </Col>
+                </Row>
+                <Row>{activeTab === "department" && <CustomTable rows={departmentRows} columns={departmentCols} type="settings" />}</Row>
+              </Tab.Pane>
+              <Tab.Pane eventKey="vehicle">
+                <Row className="d-flex align-items-center mb-2">
+                  <Col lg={6} className="">
+                    <h2 className="text-primary thin-text text-start">All Vehicle</h2>
+                  </Col>
+                  <Col lg={6} className="">
+                    <FloatingLabel controlId="floatingSelect" label="Status" className="small-input">
+                      <Form.Select name="statusFilter" aria-label="Floating label select example">
+                        <option>Open this select menu</option>
+                        <option value="all">All</option>
+                        <option value="ongoing">Ongoing</option>
+                        <option value="upcoming">Upcoming</option>
+                        <option value="past">Past</option>
+                      </Form.Select>
+                    </FloatingLabel>
+                  </Col>
+                </Row>
+                <Row>{activeTab === "vehicle" && <CustomTable rows={vehicleRows} columns={vehicleCols} type="settings" />}</Row>
+              </Tab.Pane>
+              <Tab.Pane eventKey="checklist">
+                <Row className="d-flex align-items-center mb-2">
+                  <Col lg={6} className="">
+                    <h2 className="text-primary thin-text text-start">All Checklist</h2>
+                  </Col>
+                  <Col lg={6} className="">
+                    <FloatingLabel controlId="floatingSelect" label="Status" className="small-input">
+                      <Form.Select name="statusFilter" aria-label="Floating label select example">
+                        <option>Open this select menu</option>
+                        <option value="all">All</option>
+                        <option value="ongoing">Ongoing</option>
+                        <option value="upcoming">Upcoming</option>
+                        <option value="past">Past</option>
+                      </Form.Select>
+                    </FloatingLabel>
+                  </Col>
+                </Row>
+                <Row>{activeTab === "checklist" && <CustomTable rows={checklistRows} columns={checklistCols} type="settings" />}</Row>
+              </Tab.Pane>
+            </Tab.Content>
+          </Tab.Container>
+        </Col>
+      </Row>
+      <CustomToast header={"Login"} body={"Login Unsuccessful"} time={"Just now"} show={showToast} setShow={setShowToast} variant={"success"} />
+    </Container>
   );
 }
